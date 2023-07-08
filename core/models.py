835554974d8bdb526235a924bca28478548cc218
gpcsantos <gpcsantos@gmail.com>
2023-07-08 11:15:51 -0300

    endpoints feitos e completos: Atrações, comentarios, core(ponto_turistico), enderecos. A segir: recursos avançados

---
+++ core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from atracoes.models import Atracao
+from comentarios.models import Comentario
+from enderecos.models import Endereco
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+    comentarios = models.ManyToManyField(Comentario)
+    endereco = models.ForeignKey(Endereco, 
+                                 on_delete=models.CASCADE,
+                                 null=True, blank=True
+                                 )
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Potos Turístico"
+        verbose_name_plural =  'Pontos Turísticos'
